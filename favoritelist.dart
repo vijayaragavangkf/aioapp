9c925a624a34a49199d73cdaa4d48de5efe20b52
theyashjha <55285327+theyashjha@users.noreply.github.com>
2019-12-14 12:08:42 +0530

    Update favoritelist.dart

--- favoritelist.dart
+++ favoritelist.dart
@@ -1,48 +1,48 @@
 import 'package:aioapp2/lists.dart';
 import 'package:flutter/material.dart';
 
-class _FavoriteListState extends State<FavoriteList> {
-  final _suggestions = [];
-  final Set<Widget> _saved = Set<Widget>();
-
-  Widget _buildList() {
-    return ListView.builder(
-      itemCount: 53,
-      itemBuilder: (BuildContext context, int index) {
-        return _buildRow(_suggestions[index], index);
-      },
-    );
-  }
-
-  Widget _buildRow(BuildContext context, int index){
-    // final bool alreadySaved = _saved.contains(website);
-    return Card(
-      child: Container(
-        child: ListTile(
-          contentPadding: EdgeInsets.symmetric(horizontal: 20, vertical: 10),
-          subtitle: Row(
-            children: <Widget>[
-              Image.asset('lib/images/${images[index]}'),
-              Text(nameOfSite[index]),
-            ],
-          ),
-          onTap: (){},
-        ),
-      ),
-    );
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      body: _buildList(),
-    );
-  }
-}
 
 class FavoriteList extends StatefulWidget {
   @override
   _FavoriteListState createState() => _FavoriteListState();
+}
+
+class _FavoriteListState extends State<FavoriteList> {
+  bool isChecked = false;
+
+  final Set _saved = Set();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: ListView.builder(
+        itemCount: 53,
+        itemBuilder: (context, index) {
+          return CheckboxListTile(
+            checkColor: Colors.indigo,
+            value: isChecked,
+            onChanged: (val) {
+              setState(() {
+                isChecked = val;
+                if(val == true){
+                  _saved.add(context);
+                } else{
+                  _saved.remove(context);
+                }
+              });
+            },
+            title: Row(
+              children: <Widget>[
+                Image.asset('lib/images/${images[index]}'),
+                SizedBox(width: 10,),
+                Text(nameOfSite[index]),
+              ],
+            ),
+          );
+        },
+      ),
+    );
+  }
 }
 
 // Scaffold(
