a65ebce3c42ae11b050e342fc9a1cb8fc4279c73
theyashjha <55285327+theyashjha@users.noreply.github.com>
2019-12-17 12:32:46 +0530

    Update favoritelist.dart

--- favoritelist.dart
+++ favoritelist.dart
@@ -1,7 +1,7 @@
 import 'package:aioapp2/lists.dart';
 import 'package:flutter/material.dart';
+import 'secondtab.dart';
 
-final Set _saved = Set();
 
 class FavoriteList extends StatefulWidget {
   @override
@@ -9,8 +9,6 @@ class FavoriteList extends StatefulWidget {
 }
 
 class _FavoriteListState extends State<FavoriteList> {
-  // bool isChecked = false;
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -27,7 +25,7 @@ class _FavoriteListState extends State<FavoriteList> {
               activeColor: Colors.red,
               checkColor: Colors.white,
               // value: _saved.contains(context), // changed
-              value: _saved.contains(index),
+              value: saved.contains(index),
               onChanged: (val) {
                 setState(() {
                   // isChecked = val;           // changed
@@ -37,15 +35,15 @@ class _FavoriteListState extends State<FavoriteList> {
                   //   _saved.remove(context);  // changed
                   // }                          // changed
                   if (val == true) {
-                    _saved.add(index);
+                    saved.add(index);
                   } else {
-                    _saved.remove(index);
+                    saved.remove(index);
                   }
                 });
-              },
+              }
               title: Row(
                 children: <Widget>[
-                  Image.asset('lib/images/${images[index]}'),
+                  Image.asset('lib/images/${images[index]}'), //have to make the FAVORITES list exactly like this in the SECOND TAB.
                   SizedBox(
                     width: 10,
                   ),
@@ -60,7 +58,7 @@ class _FavoriteListState extends State<FavoriteList> {
         foregroundColor: Colors.red,
         child: Icon(Icons.check),
         onPressed: () {
-          Navigator.pop(context, _saved);
+          Navigator.pop<Set>(context, saved);
         },
       ),
     );
